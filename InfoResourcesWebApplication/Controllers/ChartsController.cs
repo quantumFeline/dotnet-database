e1c50ba095ccabb9a028bf37fd58690f10f92c86
Veronika Nechaieva <veronika.netch@gmail.com>
2021-05-03 22:38:53 +0300

    Add a second chart

--- InfoResourcesWebApplication/Controllers/ChartsController.cs
+++ InfoResourcesWebApplication/Controllers/ChartsController.cs
@@ -33,5 +33,25 @@ namespace InfoResourcesWebApplication.Controllers
 
             return new JsonResult(types_to_resources);
         }
+
+        [HttpGet("JsonDepartmentStats")]
+        public JsonResult JsonDepartmentStats()
+        {
+            List<Department> departments = _context.Department.Include(a => a.Authors).ToList();
+            List<object> department_competition = new List<object>();
+            department_competition.Add(new string[] { "Тип ресурсу", "Кількість ресурсів" });
+            var author_resources = _context.Author.Include(r => r.Resources).ToDictionary(a => a.AuthorId, a => a.Resources.Count);
+            foreach (var department in departments)
+            {
+                var dep_authors = department.Authors;
+                var total_articles = 0;
+                foreach (var author in dep_authors) {
+                    total_articles += author_resources[author.AuthorId];
+                }
+                department_competition.Add(new object[] { department.DepartmentName, total_articles});
+            }
+
+            return new JsonResult(department_competition);
+        }
     }
 }
