bf2d3600761b9e08d407bf1f81e38a5a22d6dd0c
Veronika Nechaieva <veronika.netch@gmail.com>
2021-05-08 15:02:38 +0300

    Successful import/export

--- InfoResourcesWebApplication/Controllers/ResourcesController.cs
+++ InfoResourcesWebApplication/Controllers/ResourcesController.cs
@@ -170,7 +170,7 @@ namespace InfoResourcesWebApplication.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Import(IFormFile fileExcel)
         {
-            if (ModelState.IsValid && fileExcel == null)
+            if (ModelState.IsValid && fileExcel != null)
             {
                 using (var stream = new FileStream(fileExcel.FileName, FileMode.Create))
                 {
@@ -178,70 +178,167 @@ namespace InfoResourcesWebApplication.Controllers
                     ImportFile(fileExcel, stream);
                 }
                 await _context.SaveChangesAsync();
+                ViewData["ImportMessage"] = "File imported succefully.";
+            } else
+            {
+                ViewData["ImportMessage"] = "Error while importing file: ";
+                if (fileExcel == null)
+                {
+                    ViewData["ImportMessage"] += "file is null";
+                } else
+                {
+                    var errors = ModelState.Where(x => x.Value.Errors.Any())
+                   .Select(x => new { x.Key, x.Value.Errors });
+                    ViewData["ImportMessage"] += "model state is invalid - " + errors;
+                }                
             }
 
-            return RedirectToAction(nameof(Index));
+            //return RedirectToAction(nameof(Index));
+            return View();
         }
 
-        public void ImportFile(IFormFile fileExcel, FileStream stream)
+        [NonAction]
+        protected void ImportFile(IFormFile fileExcel, FileStream stream)
         {
             
             using (XLWorkbook workBook = new XLWorkbook(stream, XLEventTracking.Disabled))
             {
                 foreach (IXLWorksheet worksheet in workBook.Worksheets)
                 {
-                    ResourceType new_res;
+                    ResourceType type;
                     List<ResourceType> types = (from res in _context.ResourceType
                                 where res.ResourceTypeName.Contains(worksheet.Name)
                                 select res).ToList();
 
                     if (types.Count > 0)
                     {
-                        new_res = types[0];
+                        type = types[0];
                     }
                     else
                     {
-                        new_res = new ResourceType();
-                        new_res.ResourceTypeName = worksheet.Name;
-                        new_res.ResourceTypeDescription = "from EXCEL";
-                        _context.ResourceType.Add(new_res);
+                        type = new ResourceType();
+                        type.ResourceTypeName = worksheet.Name;
+                        type.ResourceTypeDescription = "from EXCEL";
+                        _context.ResourceType.Add(type);
                     }
                     
                     foreach (IXLRow row in worksheet.RowsUsed().Skip(1))
                     {
-                        try
+                        //try
                         {
                             Resource resource = new Resource();
                             resource.ResourceName = row.Cell(1).Value.ToString();
-                            resource.Annotation = row.Cell(4).Value.ToString();
+                            resource.Type = type.ResourceTypeId;
+                            resource.UrlAddress = row.Cell(5).Value.ToString();
+                            resource.Annotation = row.Cell(6).Value.ToString();
+
+                            var add_date = row.Cell(7).Value.ToString();
+                            if (add_date.Length > 0)
+                            {
+                                resource.AddDate = DateTime.Parse(add_date);
+                            } else
+                            {
+                                resource.AddDate = DateTime.Now;
+                            }
                             _context.Resource.Add(resource);
 
                             // Author
                             string authorName = row.Cell(2).Value.ToString();
+                            string authorDepartment = row.Cell(3).Value.ToString();
                             if (authorName.Length > 0)
                             {
-                                var a = (from aut in _context.Author
+                                var a = (from aut in _context.Author.ToList()
                                             where aut.FullName.Contains(authorName)
                                             select aut).ToList();
 
                                 if (a.Count > 0)
                                 {
-                                    resource.Author = a[0].AuthorId;
+                                    resource.Author = a[0] .AuthorId;
                                 }
                                 else
                                 {
-                                    throw new InvalidDataException("Author not found in the database");
+                                    Author author = new Author();
+                                    var names = "surname, name patronymic".Split(new char[]{ ',', ' '});
+                                    if (names.Length != 3)
+                                    {
+                                        throw new InvalidDataException("Author name can't be parsed");
+                                    }
+                                    author.LastName = names[0];
+                                    author.FirstName = names[2]; // skip the empty entry
+                                    author.Patronymic = names[3];
+
+                                    // Set or add department
+                                    Department department;
+                                    List<Department> departments = (from d in _context.Department
+                                                                where d.DepartmentName.Contains(worksheet.Name)
+                                                                select d).ToList();
+
+                                    if (departments.Count > 0) {
+                                        department = departments[0];
+                                    }
+                                    else {
+                                        throw new InvalidDataException("Unknown author department");
+                                    }
+
+                                    author.Department = department.DepartmentId;
+                                    _context.Author.Add(author);
+
+                                    resource.Author = author.AuthorId;
                                 }
                             }
-
-                            // ResourceType
                         }
-                        catch (Exception e)
+                        /*catch (Exception)
                         {
-                            //logging самостійно :)
-
-                        }
+                            throw new FileLoadException("Error while importing a database");
+                        }*/
                     }
+                }
+            }
+        }
+
+        public ActionResult Export()
+        {
+            using (XLWorkbook workbook = new XLWorkbook(XLEventTracking.Disabled))
+            {
+                var resourceTypes = _context.ResourceType.Include(n => n.Resources).ToList();
+                foreach (var type in resourceTypes)
+                {
+                    var worksheet = workbook.Worksheets.Add(type.ResourceTypeName);
+
+                    worksheet.Cell("A1").Value = "Назва";
+                    worksheet.Cell("B1").Value = "Автор";
+                    worksheet.Cell("C1").Value = "Факультет";
+                    worksheet.Cell("D1").Value = "Тип ресурсу";
+                    worksheet.Cell("E1").Value = "URL";
+                    worksheet.Cell("F1").Value = "Аннотація";
+                    worksheet.Cell("G1").Value = "Дата";
+                    worksheet.Row(1).Style.Font.Bold = true;
+
+                    List<Resource> resources = type.Resources.ToList();
+
+                    for (int i = 0; i < resources.Count; i++)
+                    {
+                        worksheet.Cell(i + 2, 1).Value = resources[i].ResourceName;
+                        Author author = _context.Author.Find(resources[i].Author);
+                        worksheet.Cell(i + 2, 2).Value = author.FullName;
+                        worksheet.Cell(i + 2, 3).Value = author.Department;
+                        ResourceType res_type = _context.ResourceType.Find(resources[i].Type);
+                        worksheet.Cell(i + 2, 4).Value = res_type.ResourceTypeName;
+                        worksheet.Cell(i + 2, 5).Value = resources[i].UrlAddress;
+                        worksheet.Cell(i + 2, 6).Value = resources[i].Annotation;
+                        worksheet.Cell(i + 2, 7).Value = resources[i].AddDate;
+                    }
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    stream.Flush();
+
+                    return new FileContentResult(stream.ToArray(),
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                    {
+                        FileDownloadName = $"resources_db_{DateTime.UtcNow.ToShortDateString()}.xlsx"
+                    };
                 }
             }
         }
