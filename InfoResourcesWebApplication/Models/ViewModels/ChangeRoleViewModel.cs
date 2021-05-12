f2c93147542a1a037289ff50c6dd9fffbae802fe
Veronika Nechaieva <veronika.netch@gmail.com>
2021-05-12 15:05:09 +0300

    Role controllers

--- InfoResourcesWebApplication/Models/ViewModels/ChangeRoleViewModel.cs
+++ InfoResourcesWebApplication/Models/ViewModels/ChangeRoleViewModel.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace InfoResourcesWebApplication.Models.ViewModels
 {
     public class ChangeRoleViewModel
     {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public List<IdentityRole> AllRoles { get; set; }
+        public IList<string> UserRoles { get; set; }
+        public ChangeRoleViewModel()
+        {
+            AllRoles = new List<IdentityRole>();
+            UserRoles = new List<string>();
+        }
     }
 }
---
+++ InfoResourcesWebApplication/RoleInitialiser.cs
@@ -0,0 +1,35 @@
+﻿using InfoResourcesWebApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoResourcesWebApplication
+{
+    public class RoleInitialiser
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "TOP_SECRET";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
